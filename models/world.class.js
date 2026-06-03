a96f0892d1b40287c7abf0edd16c9636a8e28b8b
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-06-03 22:12:30 +0200

    verbessere die Zeichenlogik im World-Klasse

--- models/world.class.js
+++ models/world.class.js
@@ -1,14 +1,24 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken(), new Chicken(), new Chicken()];
+  enemies = [new Chicken(), new Chicken()];
 
   ctx;
+  canvas;
+
   constructor(canvas) {
+    this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.draw();
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
     this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.widht, this.character.height);
+    this.enemies.forEach(enemy=>{
+        this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.widht, enemy.height);
+    })
+
+    requestAnimationFrame(() => this.draw());
   }
 }
